89bdde7918d43dedf5fda8c9dc89d295dfbb651c
Lektro <larskr93@gmail.com>
2019-09-22 15:56:59 +0200

    change the local ip in settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b%)o)3fqcn6+%6=(=wa)39g1lfbf*y9z7j+%pwhnq_v#sodq$p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['192.168.178.102']
 
 TINYMCE_DEFAULT_CONFIG = {
     'height': 360,
